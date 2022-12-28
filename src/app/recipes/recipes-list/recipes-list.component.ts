285b7c9322206924b8f067ef5a05657143e5d473
miguelmaroune <miguelmaroune@gmail.com>
2022-12-28 20:08:26 +0200

    Starting Project Set up

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Recipe} from '../recipes.model'
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent {
+
+recipes:Recipe[]  = [
+ new Recipe('A test Recipe','This is simply a test ','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
+  ];
 
 }
--- src/app/shopping-list/shoping-list.component.ts
+++ src/app/shopping-list/shoping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shoping-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent {
+ingredients:Ingredient[] = [
+  new Ingredient('Apples',5),
+  new Ingredient('Tomatoes',10)];
 
 }
